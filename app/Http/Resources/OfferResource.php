0d1faf4b6586d3e0e9e5960fac350366f6239099
Zhetenov Ralan <rollwho@gmail.com>
2021-04-19 17:32:50 +0600

    fixed to city

--- app/Http/Resources/OfferResource.php
+++ app/Http/Resources/OfferResource.php
@@ -27,7 +27,7 @@ class OfferResource extends JsonResource
             'from_price'    => $this->from_price,
             'to_price'      => $this->to_price,
             'rooms'         => $this->rooms,
-            'city'          => new CityResource($this->region),
+            'city'          => new CityResource($this->region->city),
             'offers'        => count($this->offerStatuses),
         ];
     }
